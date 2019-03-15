6ca5a650c32409a759c2888ba7f09b77e28bf10a
denismartens <dnismartens@gmail.com>
2019-03-14 21:58:10 -0400

    update create thumbnails script to use new directory structure

--- create_thumbnails.rb
+++ create_thumbnails.rb
@@ -17,15 +17,20 @@ end.parse!
 raise 'Directory not provided, please provide a directory' if options[:path].nil?
 bucket = AWS::S3::Bucket.find(ENV['AWS_BUCKET_NAME'])
 images_dir = "http://s3.amazonaws.com/#{bucket.name}"
-bucket.objects(prefix: options[:path]).each do |img|
+bucket.objects(prefix: options[:path] + '/slides').each do |img|
   # skip object if not an image or is already a thumbnail image
-	next if img.about['content-type'] != 'image/jpeg' || img.key.include?('thumbnail')
-	thumbnail_name = img.key.gsub(/\/(?<name>.*)$/,'/thumbnail_\k<name>')
+	next if img.about['content-type'] != 'image/jpeg'
   # skip object if its thumbnail version already exists and replace option is false
-	next if AWS::S3::S3Object.exists?(thumbnail_name, bucket.name) && !options[:replace]
+  thumbnail_path = img.key.gsub(/slides\/(?<name>.*)$/, 'thumbnails/\k<name>')
+	if AWS::S3::S3Object.exists?(thumbnail_path, bucket.name)
+    if options[:replace]
+      AWS::S3::S3Object.delete(thumbnail_path, bucket.name)
+    else
+      next
+    end
+  end
   # create new thumbnail image and overwrite existing if needed
-	thumbnail_img = MiniMagick::Image.open(File.join(images_dir, img.key)).resize('350')
-  AWS::S3::S3Object.delete(thumbnail_name, bucket.name) if AWS::S3::S3Object.exists?(thumbnail_name, bucket.name)
-  AWS::S3::S3Object.store(thumbnail_name, thumbnail_img.to_blob, bucket.name)
-  puts "Created thumbnail for #{img.key}"
+	thumbnail_img = MiniMagick::Image.open(File.join(images_dir, img.key)).resize('400')
+  AWS::S3::S3Object.store(thumbnail_path, thumbnail_img.to_blob, bucket.name, content_type: 'image/jpeg')
+  puts "Created thumbnail #{thumbnail_path} for #{img.key}"
 end
