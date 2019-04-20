d40fff03d4f5502a9bd60fe38967402ac7eba1ad
denismartens <dnismartens@gmail.com>
2019-04-19 22:20:19 -0400

    when creating thumbnails, replace option should mean delete all first

--- create_thumbnails.rb
+++ create_thumbnails.rb
@@ -17,17 +17,21 @@ end.parse!
 raise 'Directory not provided, please provide a directory' if options[:path].nil?
 bucket = AWS::S3::Bucket.find(ENV['AWS_BUCKET_NAME'])
 images_dir = "http://s3.amazonaws.com/#{bucket.name}"
+if options[:replace]
+  bucket.objects(prefix: options[:path] + '/thumbnails')
+        .keep_if{|obj| obj.about['content-type'] == 'image/jpeg'}
+        .each do |img|
+          AWS::S3::S3Object.delete(img.key, bucket.name)
+          puts "Deleted thumbnail #{img.key}"
+  end
+end
 bucket.objects(prefix: options[:path] + '/slides').each do |img|
   # skip object if not an image or is already a thumbnail image
 	next if img.about['content-type'] != 'image/jpeg'
   # skip object if its thumbnail version already exists and replace option is false
   thumbnail_path = img.key.gsub(/slides\/(?<name>.*)$/, 'thumbnails/\k<name>')
 	if AWS::S3::S3Object.exists?(thumbnail_path, bucket.name)
-    if options[:replace]
-      AWS::S3::S3Object.delete(thumbnail_path, bucket.name)
-    else
-      next
-    end
+    next
   end
   # create new thumbnail image and overwrite existing if needed
 	thumbnail_img = MiniMagick::Image.open(File.join(images_dir, img.key)).resize('600')
